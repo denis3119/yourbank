35b80a967017a861fa8d4a27554eb305ce422e84
denis3119 <denis3119@gmail.com>
2016-01-13 01:54:33 +0300

    имя usercredit

--- src/main/java/com/yourbank/data/model/user/UserCredit.java
+++ src/main/java/com/yourbank/data/model/user/UserCredit.java
@@ -29,6 +29,8 @@ public class UserCredit extends AbstractExpiringEntity {
     @OneToOne(cascade = CascadeType.PERSIST)
     private Score score;
 
+    private String name;
+
     private double percent;
     @ManyToOne
     private User user;
@@ -41,6 +43,7 @@ public class UserCredit extends AbstractExpiringEntity {
     public UserCredit(UserCredit credit, User user, double sum, int term) {
         setDescription(credit.getDescription());
         setCurrency(credit.getCurrency());
+        setName(credit.getName());
         setPercent(credit.getPercent());
         this.user = user;
         this.sum = sum;
